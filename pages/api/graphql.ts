8f20d51e4fb1c54a1686aa16d44893e4563d6eac
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-02 08:28:19 -0400

    graphql api

---
+++ pages/api/graphql.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from "apollo-server-micro";
+import resolvers from '../../src/api/resolvers';
+import typeDefs from '../../src/api/typedefs';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default server.createHandler({ path: "/api/graphql" })
