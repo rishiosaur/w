faa9624ef92a630b764c9a85c2130a2756b299df
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-04 11:56:33 -0400

    prettier

--- src/containers/BaseContainer.tsx
+++ src/containers/BaseContainer.tsx
@@ -1,12 +1,12 @@
 import { Flex, Text } from '@chakra-ui/core'
 
 const BaseContainer: React.FC = ({ children }) => (
-		<>
-			{/* <Flex> */}
+	<>
+		{/* <Flex> */}
 
-			<Flex flex="1 1 auto">{children}</Flex>
-			{/* </Flex> */}
-		</>
-	)
+		<Flex flex="1 1 auto">{children}</Flex>
+		{/* </Flex> */}
+	</>
+)
 
 export default BaseContainer
