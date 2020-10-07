2fb72b8145ca1a8abd7e3eb3b73a2ce4bd21e058
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-06 21:18:02 -0400

    try something out

--- src/functions/fetch.ts
+++ src/functions/fetch.ts
@@ -57,6 +57,8 @@ const {
 	SPOTIFY_REFRESH: refresh_token,
 } = process.env
 
+console.log(process.env)
+
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
