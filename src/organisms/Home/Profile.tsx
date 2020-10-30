8c010d46456b1249135f2c6f5a3a7a24af72ed0e
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-30 17:02:17 -0400

    Update Profile.tsx

--- src/organisms/Home/Profile.tsx
+++ src/organisms/Home/Profile.tsx
@@ -132,7 +132,7 @@ export function Profile({ spotify }) {
 						'resume',
 					],
 					[
-						'https://z.rishi.cx/dir',
+						'https://z.rishi.cx/ls',
 						'https://source.unsplash.com/mtf9jIj2vg0/80x80',
 						'links',
 					],
